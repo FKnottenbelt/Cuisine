91f91d848a57481f07ce1de420e345fd8d8b007f
Femke Knottenbelt <femknotgithub@gmail.com>
2020-01-10 19:01:56 +0000

    Setting up the project

---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 8080);
+
+// template engine
+app.set("view engine", "ejs");
+const layouts = require("express-ejs-layouts");
+app.use(layouts);
+
+// contollers
+const homeController = require("./controllers/homeController");
+
+// middleware
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.json());
+
+// routes
+app.get('/', homeController.home);
+app.get('/courses', homeController.showCourses);
+app.get('/contact', homeController.showSignUp);
+app.post('/contact', homeController.postedSignUpForm);
+
+// server
+app.listen(app.get('port'), () => {
+  console.log(`Server is running at port ${app.get('port')}`);
+});
